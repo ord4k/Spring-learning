b321df2706ada65e4b3cdd669dbb4b3ca9663b63
ord4k <michal.ordak@gmail.com>
2016-12-29 19:40:46 +0100

    Spring MVC by caveofprograming
    Completed

---
+++ spring-tutorial-BasicMVC-end/src/com/caveofprogramming/spring/web/controllers/OffersController.java
@@ -0,0 +1,39 @@
+package com.caveofprogramming.spring.web.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+import com.caveofprogramming.spring.web.service.OffersService;
+
+@Controller
+public class OffersController {
+	
+	private OffersService offersService;
+	
+	@Autowired
+	public void setOffersService(OffersService offersService) {
+		this.offersService = offersService;
+	}
+
+
+	@RequestMapping(value="/test", method=RequestMethod.GET)
+	public String showTest(Model model, @RequestParam("id") String id) {
+		
+		System.out.println("Id is " + id);
+		return "home";
+	}
+	@RequestMapping("/createoffer")
+	public String createOffers() {
+
+		return "createoffer";
+	}
+	
+
+}
