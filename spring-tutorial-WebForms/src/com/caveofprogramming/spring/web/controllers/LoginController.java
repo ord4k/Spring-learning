f08f5195aeecae2e0ce3d8d7c4bbcd3e3640b6c6
ord4k <michal.ordak@gmail.com>
2017-02-22 22:01:44 +0100

    LoginCOntroller updated to support validation group

--- spring-tutorial-WebForms/src/com/caveofprogramming/spring/web/controllers/LoginController.java
+++ spring-tutorial-WebForms/src/com/caveofprogramming/spring/web/controllers/LoginController.java
@@ -9,9 +9,11 @@ import org.springframework.dao.DuplicateKeyException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.caveofprogramming.spring.web.dao.FormValidationGroup;
 import com.caveofprogramming.spring.web.dao.User;
 
 import com.caveofprogramming.spring.web.service.UsersService;
@@ -62,7 +64,7 @@ public class LoginController {
 	}
 
 	@RequestMapping(value = "/createaccount", method = RequestMethod.POST)
-	public String doCreate(@Valid User user, BindingResult result) {
+	public String doCreate(@Validated(FormValidationGroup.class) User user, BindingResult result) {
 
 		if (result.hasErrors()) {
 			return "newaccount";
